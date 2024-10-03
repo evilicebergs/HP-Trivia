0594ae5ca52041eaff8db553b0738557ff90ca9a
evilicebergs <voiced.fair-0a@icloud.com>
2024-10-03 18:46:51 +0300

    Initial Commit

---
+++ HP Trivia/HP_TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HP_TriviaApp.swift
+//  HP Trivia
+//
+//  Created by Artem Golovchenko on 2024-10-03.
+//
+
+import SwiftUI
+
+@main
+struct HP_TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
